004c3de62e43315eb07890cb9910833276da936b
Ryan <ryansteffan.biz@gmail.com>
2024-05-21 00:36:11 -0500

    Updated buffer.py: Added add_validated_item, append, and flush methods.
    Signed-off-by: Ryan <ryansteffan.biz@gmail.com>

--- src/buffer.py
+++ src/buffer.py
@@ -1,7 +1,6 @@
 import time
 import typing
 
-
 class Buffer:
 
     def __init__(self, type: typing.Type, max_size: int, *args) -> None:
@@ -11,20 +10,23 @@ class Buffer:
         self.last_append_time = self.creation_time
         self.buffer_items = []
         for item in args:
-            if isinstance(item, self.buffer_type):
-                self.buffer_items.append(item)
-            else:
-                raise TypeError(f"Item {item} is not of the type "
-                                f"{self.buffer_type}")
-            if len(self.buffer_items) >= self.max_size:
-                raise OverflowError("A buffer may not be initialized with more "
-                                    "items in the buffer than max size set.")
+            self.add_validated_item(item)
 
-    def append(self):
-        ...
+
+    def append(self, value):
+        self.add_validated_item(value)
 
     def flush(self):
-        ...
+        self.buffer_items = []
+
+    def add_validated_item(self, item):
+        if not isinstance(item, self.buffer_type):
+            raise TypeError(f"Item {item} is not of the type "
+                            f"{self.buffer_type}")
+        if len(self.buffer_items) >= self.max_size:
+            raise OverflowError("A buffer may not be initialized with more "
+                                "items in the buffer than max size set.")
+        self.buffer_items.append(item)
 
     def __iter__(self):
         ...
